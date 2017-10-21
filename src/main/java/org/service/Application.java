8ef129b3e035d7ad656f2bdfd4e86a51588562c7
pavansachi <pavan8sachi@gmail.com>
2017-10-22 09:11:21 +1100

    spring boot main class

---
+++ src/main/java/org/service/Application.java
@@ -0,0 +1,22 @@
+package org.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Main boot application
+ * @author pavansachi
+ *
+ */
+
+@SpringBootApplication
+@ComponentScan(basePackages={"org.controllers"})
+public class Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Application.class, args);		
+	}
+
+}
