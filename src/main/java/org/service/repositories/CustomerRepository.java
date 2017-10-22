7bcba52b26c24135d7397504fec962b86e564ca2
pavansachi <pavan8sachi@gmail.com>
2017-10-22 23:13:21 +1100

    customer repository added with fetch capabilities

--- src/main/java/org/service/repositories/CustomerRepository.java
+++ src/main/java/org/service/repositories/CustomerRepository.java
@@ -1,10 +1,15 @@
 package org.service.repositories;
 
 import java.io.Serializable;
+import java.util.List;
 
-import org.models.domain.Customer;
+import org.models.domain.CustomerRecord;
 import org.springframework.data.repository.CrudRepository;
 
-public interface CustomerRepository extends CrudRepository<Customer, Serializable>  {
+public interface CustomerRepository extends CrudRepository<CustomerRecord, Serializable>  {
 
+	List<CustomerRecord> findByCustomerIdOrderByDateAscAmountAsc(long customerId);
+	List<CustomerRecord> findByCustomerIdAndMonthAndYearOrderByDateAscAmountAsc(long customerId, int month, int year);
+	List<CustomerRecord> findByCustomerIdAndYearOrderByDateAscAmountAsc(long customerId, int year);
+	
 }
